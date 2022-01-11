91c9a7f085a0e38eb183786df879f4e69521decd
harsh21104116 <96731849+harsh21104116@users.noreply.github.com>
2022-01-11 15:19:37 +0530

    Add files via upload

---
+++ computing assignment input.py
@@ -0,0 +1,32 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:\COMPUTING\Computing Assignment.py ================
+To find avgreges of three numbers
+Enter num1 :54
+Enter num2 :24
+Enter num3 :254
+Average: 110.66666666666667
+To compute persson income tax
+Tax Rate=20%
+Standard Deduction=$10000
+Dependent Deduction=$3000
+Enter Gross Income :3020000
+Enter number of dependents :54
+Taxable Income: 2848000
+Tax: 569600.0
+Different data type values into a list
+[21104116, 'Harsh', 'M', 'B.Tech', 8.5]
+[21104568, 'Kartikay', 'M', 'B,Tech', 8.3]
+[25640025, 'Sarika', 'F', 'BSc', 7.9]
+[15689423, 'Diksha', 'F', 'B.A', 8.2]
+Marks of 5 students into a list and display them in sorted manner
+Marks of students in computing
+[100, 58, 79, 15, 62]
+Ascending Order: [15, 58, 62, 79, 100]
+Descending Order: [100, 79, 62, 58, 15]
+To remove an item from list
+List before removing: ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+List after removing: ['Red', 'Green', 'White', 'Pink', 'Yellow']
+To Remove Black and Pink from the list and replace them with Purple
+Replaced List: ['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
